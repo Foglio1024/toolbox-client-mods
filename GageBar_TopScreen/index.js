9dc1ce5a74e15d1118f09b8b6799f9a2bec5a016
Foglio1024 <foglio1024@gmail.com>
2020-08-05 11:20:50 +0200

    new API

--- GageBar_TopScreen/index.js
+++ GageBar_TopScreen/index.js
@@ -1,3 +1,11 @@
-module.exports = function GageBar_TopScreen(m){
-    m.installGPK("S1UI_GageBar.gpk");
+exports.ClientMod = class
+{
+    constructor(mod)
+    {
+        this.install = function(installer)
+        {
+            if(mod.clientInterface.arch == 'x64') return;
+            installer.gpk("S1UI_GageBar.gpk");
+        };
+    }
 }
